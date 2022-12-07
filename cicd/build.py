9ca9aeaa5152f1bdf12a0822c5d19052d4847040
laupse <lgrondin@ingeniance.fr>
2022-12-07 13:59:06 +0100

    Fix build and push arg to build

--- cicd/build.py
+++ cicd/build.py
@@ -1,53 +1,65 @@
 import os
 import sys
-import anyio
 import dagger
 
 
-def build():
+def main():
     with dagger.Connection(dagger.Config(log_output=sys.stdout, execute_timeout=1800)) as client:
-        cargo = client.host().directory(
-            ".", include=["Cargo*"])
 
-        build = (client
-                 .container()
-                 .from_("rust:1.63.0")
-                 .with_env_variable("USER", "root")
-                 .exec(["cargo", "new", "--bin", "ecowatt-twitter-bot"]))
+        final = build(client)
+        if len(sys.argv) == 2 and sys.argv[1] == "--push":
 
-        build = (build
-                 .with_workdir("/ecowatt-twitter-bot")
-                 .with_directory(".", cargo)
-                 .exec(["cargo", "build", "--release"]))
+            image_ref = os.getenv("IMAGE_REF")
+            if image_ref is not None:
+                final.publish(image_ref)
+                return
 
-        src = (client
-               .host()
-               .directory(".", include=["src"]))
+            image_registry = os.getenv("IMAGE_REGISTRY")
+            image_tag = os.getenv("IMAGE_TAG")
+            if image_registry is not None:
+                final.publish(image_ref.format(
+                    "%s/ecowatt-twitter-bot:%s", image_registry, image_tag))
+                return
 
-        executable = (build
-                      .with_directory(".", src)
-                      .exec(["cargo", "build", "--release"])
-                      .file("/ecowatt-twitter-bot/target/release/ecowatt-twitter-bot"))
+            print("IMAGE_REF or IMAGE_REGISTRY+IMAGE_TAG env is required")
 
-        final = (client.container()
-                 .from_("rust:1.63.0-slim")
-                 .exec(["apt-get update"])
-                 .exec(["apt-get", "install", "-y", "dialog", "apt-utils", "tzdata"])
-                 .exec(["rm", "/etc/localtime"])
-                 .exec(["ln", "-s", "/usr/share/zoneinfo/Europe/Paris /etc/localtime"])
-                 .with_file(".", executable))
 
-        image_ref = os.getenv("IMAGE_REF")
-        if image_ref is not None:
-            str = final.publish(image_ref)
-            return
+def build(client: dagger.Client):
+    cargo = (client
+             .host()
+             .directory(".", include=["Cargo*"]))
 
-        image_registry = os.getenv("IMAGE_REGISTRY")
-        image_tag = os.getenv("IMAGE_TAG")
-        if image_registry is not None:
-            build.publish(image_ref.format(
-                "%s/ecowatt-twitter-bot:%s", image_registry, image_tag))
+    build = (client
+             .container()
+             .from_("rust:1.63.0")
+             .with_env_variable("USER", "root")
+             .exec(["cargo", "new", "--bin", "ecowatt-twitter-bot"]))
+
+    build = (build
+             .with_workdir("/ecowatt-twitter-bot")
+             .with_directory(".", cargo)
+             .exec(["cargo", "build", "--release"]))
+
+    src = (client
+           .host()
+           .directory(".", include=["src"]))
+
+    executable = (build
+                  .with_directory(".", src)
+                  .exec(["cargo", "build", "--release"])
+                  .file("/ecowatt-twitter-bot/target/release/ecowatt-twitter-bot"))
+
+    final = (client.container()
+             .from_("rust:1.63.0-slim")
+             .exec(["apt-get", "update"])
+             .exec(["apt-get", "install", "-y", "dialog", "apt-utils", "tzdata"])
+             .exec(["rm", "/etc/localtime"])
+             .exec(["ln", "-s", "/usr/share/zoneinfo/Europe/Paris /etc/localtime"])
+             .with_file(".", executable))
+
+    final.stdout()
+    return final
 
 
 if __name__ == "__main__":
-    build()
+    main()
